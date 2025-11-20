53293361200e8a7379bfc89b67a66df14b7e252b
MDT <bonkwizardturner@gmail.com>
2025-11-19 23:30:00 -0600

    yes

---
+++ PythonRepl/arrays.py
@@ -0,0 +1,17 @@
+weapons = ["Flathead Sword"]
+inventory = []
+
+optionList = ["introOne", 
+              "introTwo", 
+              "introThree", 
+              "sceneTwo_Two",
+              "sceneTwo_Three",
+              "sceneTwo_Sub_One",
+              "sceneTwo_Sub_Two",
+              "sceneThree_One",
+              "sneakyFight_Three",
+              "sceneFive_ID",
+              "sceneFive_Shrine",
+              "shrine_Two",
+              "shrine_Three",
+              "shrine_Four"]
